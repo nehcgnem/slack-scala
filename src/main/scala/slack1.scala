97005f136b38a91c07ce77726ba6fe17d829dcc5
nehcgnem <35271275+nehcgnem@users.noreply.github.com>
2019-08-22 23:21:37 -0400

    tested communication with channel; TBD: files upload, response parse, add plot library

--- src/main/scala/slack1.scala
+++ src/main/scala/slack1.scala
@@ -5,13 +5,14 @@ import akka.actor.ActorSystem
 
 import java.nio.file.{Files, Paths}
 
+//REF：
+//https://github.com/slack-scala-client/slack-scala-client/blob/master/src/main/scala/slack/api/BlockingSlackApiClient.scala#L28-L324
 
 object slack1 {
   val token = "xoxp-692855199414-679295005154-723123930086-9a40af5ff4e04742b33f374a8d2e1481"
   val client = BlockingSlackApiClient(token)
   implicit val system = ActorSystem("slack")
-  val res = client.listChannels() // => Future[Seq[Channel]]
-
+  client.postChatMessage("general", "hello")
   def main(args: Array[String]): Unit = {
     println("hell0")
   }
