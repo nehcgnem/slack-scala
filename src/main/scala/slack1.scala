4b1f87569dd53724cce1690633daffc613b485a2
nehcgnem <35271275+nehcgnem@users.noreply.github.com>
2019-08-27 00:29:14 -0400

    test with file upload but not compatible

--- src/main/scala/slack1.scala
+++ src/main/scala/slack1.scala
@@ -1,20 +1,44 @@
 
-import slack.api.SlackApiClient          // Async
-import slack.api.BlockingSlackApiClient  // Blocking
+import slack.api.SlackApiClient
+import slack.api.BlockingSlackApiClient
 import akka.actor.ActorSystem
+import vegas._
+import java.io.File
 
+import vegas.render.WindowRenderer._
 import java.nio.file.{Files, Paths}
+
+import scala.concurrent.ExecutionContext
 
 //REF：
 //https://github.com/slack-scala-client/slack-scala-client/blob/master/src/main/scala/slack/api/BlockingSlackApiClient.scala#L28-L324
 
 object slack1 {
-  val token = "xoxp-692855199414-679295005154-723123930086-9a40af5ff4e04742b33f374a8d2e1481"
+  val token = "xoxp-692855199414-679295005154-739139132112-22fa8bedfef0002be6a4708a2744de27"
   val client = BlockingSlackApiClient(token)
   implicit val system = ActorSystem("slack")
+  implicit val ec = ExecutionContext.global
   client.postChatMessage("general", "hello")
   def main(args: Array[String]): Unit = {
     println("hell0")
+
+
+
+    val plot = Vegas("Country Pop").
+      withData(
+        Seq(
+          Map("country" -> "USA", "population" -> 314),
+          Map("country" -> "UK", "population" -> 64),
+          Map("country" -> "DK", "population" -> 80)
+        )
+      ).
+      encodeX("country", Nom).
+      encodeY("population", Quant).
+      mark(Bar)
+
+    val myObj = new File("C:/Users/megchen/Pictures/R0000114.jpg")
+    client.uploadFile(myObj)
+
   }
 
 }
