34100431318320cc969b83f28642769b104bd43d
Mengchen Shi <mengchens@gmail.com>
2019-08-30 16:52:08 -0400

    Update slack1.scala
    Plot using scala plot and upload to Slack channel using Async interface
    Removed token for security reason

--- src/main/scala/slack1.scala
+++ src/main/scala/slack1.scala
@@ -1,43 +1,30 @@
-
 import slack.api.SlackApiClient
 import slack.api.BlockingSlackApiClient
 import akka.actor.ActorSystem
-import vegas._
+
+import scala.concurrent._
+import org.sameersingh.scalaplot.Implicits._
 import java.io.File
 
-import vegas.render.WindowRenderer._
-import java.nio.file.{Files, Paths}
-
-import scala.concurrent.ExecutionContext
 
 //REF：
 //https://github.com/slack-scala-client/slack-scala-client/blob/master/src/main/scala/slack/api/BlockingSlackApiClient.scala#L28-L324
 
 object slack1 {
-  val token = "xoxp-692855199414-679295005154-739139132112-22fa8bedfef0002be6a4708a2744de27"
-  val client = BlockingSlackApiClient(token)
-  implicit val system = ActorSystem("slack")
-  implicit val ec = ExecutionContext.global
-  client.postChatMessage("general", "hello")
+
   def main(args: Array[String]): Unit = {
     println("hell0")
+    val token = ???
+    val client = SlackApiClient(token)
+    implicit val system = ActorSystem("slack")
+    //    client.postChatMessage("images", "hello from scala")
 
+    val x = 0.0 until 2.0 * math.Pi by 0.1
+    output(ASCII, xyChart(x -> (math.sin(_), math.cos(_))))
 
-
-    val plot = Vegas("Country Pop").
-      withData(
-        Seq(
-          Map("country" -> "USA", "population" -> 314),
-          Map("country" -> "UK", "population" -> 64),
-          Map("country" -> "DK", "population" -> 80)
-        )
-      ).
-      encodeX("country", Nom).
-      encodeY("population", Quant).
-      mark(Bar)
-
-    val myObj = new File("C:/Users/megchen/Pictures/R0000114.jpg")
-    client.uploadFile(myObj)
+    output(PNG("/home/ubun/Documents/", "test"), xyChart(x -> (math.sin(_), math.cos(_))))
+    val file = new File("/home/ubun/Documents/test.png")
+    client.uploadFile(Left(file), "png", "test image", "test image", "no comment", Seq("images"))
 
   }
 
