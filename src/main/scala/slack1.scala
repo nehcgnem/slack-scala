befe9fdd3db2a1aeb260fc94783b4fe5cf1807bd
nehcgnem <35271275+nehcgnem@users.noreply.github.com>
2019-09-11 20:04:56 -0400

    Verified

--- src/main/scala/slack1.scala
+++ src/main/scala/slack1.scala
@@ -12,19 +12,21 @@ import java.io.File
 
 object slack1 {
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String])  {
     println("hell0")
     val token = ???
     val client = SlackApiClient(token)
     implicit val system = ActorSystem("slack")
-    //    client.postChatMessage("images", "hello from scala")
+        client.postChatMessage("general", "hello from home")
+    println("hell0")
 
-    val x = 0.0 until 2.0 * math.Pi by 0.1
-    output(ASCII, xyChart(x -> (math.sin(_), math.cos(_))))
 
-    output(PNG("/home/ubun/Documents/", "test"), xyChart(x -> (math.sin(_), math.cos(_))))
-    val file = new File("/home/ubun/Documents/test.png")
-    client.uploadFile(Left(file), "png", "test image", "test image", "no comment", Seq("images"))
+    //    val x = 0.0 until 2.0 * math.Pi by 0.1
+//    output(ASCII, xyChart(x -> (math.sin(_), math.cos(_))))
+//
+//    output(PNG("/home/ubun/Documents/", "test"), xyChart(x -> (math.sin(_), math.cos(_))))
+//    val file = new File("/home/ubun/Documents/test.png")
+//    client.uploadFile(Left(file), "png", "test image", "test image", "no comment", Seq("images"))
 
   }
 
